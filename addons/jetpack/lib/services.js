ce1876ac30f5b1f845a0276c803decf074475c3f
mixedpuppy <mixedpuppy@gmail.com>
2011-07-05 16:41:54 -0700

    contributors we are

--- addons/jetpack/lib/services.js
+++ addons/jetpack/lib/services.js
@@ -20,6 +20,8 @@
  * Contributor(s):
  *  Michael Hanson <mhanson@mozilla.com>
  *	Anant Narayanan <anant@kix.in>
+ *	Mark Hammond <mhammond@mozilla.com>
+ *	Shane Caraveo <scaraveo@mozilla.com>
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
