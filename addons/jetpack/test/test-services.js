1cd36e069290f745cf73ca86d3c699806ab8c56b
mixedpuppy <mixedpuppy@gmail.com>
2011-08-16 13:15:24 -0700

    recommit test changes that were lot in merge

--- addons/jetpack/test/test-services.js
+++ addons/jetpack/test/test-services.js
@@ -1,4 +1,4 @@
-const {MediatorPanel} = require("services");
+const services = require("services");
 const FFRepoImpl = require("api").FFRepoImplService;
 const {getOWA, installTestApp} = require("./helpers/helpers");
 const {Cc, Ci, Cm, Cu, components} = require("chrome");
@@ -13,40 +13,46 @@ function getContentWindow() {
   return element.contentWindow.wrappedJSObject;
 }
 
-// A subclass of the mediator used for testing.
-function TestMediatorPanel(window, contentWindowRef, methodName, args, successCB, errorCB) {
-  MediatorPanel.call(this, window, contentWindowRef, methodName, args, successCB, errorCB);
-}
-TestMediatorPanel.prototype = {
-  __proto__: MediatorPanel.prototype,
+// We still use service2.html, but use different content scripts tailored
+// for testing.
+let contentScriptSuccess =
+  "window.navigator.apps.mediation.ready(function(method, args, services) {" +
+  "  let service = services[0];" +
+  "  document.getElementById('servicebox').appendChild(service.iframe);" +
+  "  service.on('ready', function() {" +
+  "    service.call('echoArgs', args, function(result) {" +
+  "      self.port.emit('result', result);" +
+  "    });" +
+  "  });" +
+  "});"
 
-  _createPopupPanel: function() {
-    // We still use service2.html, but use a different content script tailored
-    // for testing.
-    let data = require("self").data;
-    let thePanel = require("panel").Panel({
-        contentURL: data.url("service2.html"),
-        contentScriptFile: [
-            data.url("mediatorapi.js"),
-        ],
-        contentScriptWhen: "start",
-        contentScript:
-         "window.navigator.apps.mediation.ready(function(method, args, services) {" +
-         "  let service = services[0];" +
-         // XXX - why is unsafeWindow needed here???
-         "  unsafeWindow.document.getElementById('servicebox').appendChild(service.iframe);" +
-         "  service.on('ready', function() {" +
-         "    service.call('echoArgs', args, function(result) {" +
-         "      self.port.emit('result', result);" +
-         "    });" +
-         "  });" +
-         "});",
-        width: 484, height: 484
-    });
-    this.panel = thePanel;
-  }
+let contentScriptError =
+  "window.navigator.apps.mediation.ready(function(method, args, services) {" +
+  "  let service = services[0];" +
+  "  document.getElementById('servicebox').appendChild(service.iframe);" +
+  "  service.on('ready', function() {" +
+  "    service.call('testErrors', args, function(result) {" +
+  "      self.port.emit('result', {code: 'test_failure', msg: 'unexpected success callback'});" +
+  "    }, function(errob) {" +
+  "      self.port.emit('result', errob);" +
+  "    });" +
+  "  });" +
+  "});"
+
+function createTestMediatorPanel(contentScript) {
+  let data = require("self").data;
+  let thePanel = require("panel").Panel({
+    id: 'test-mediator-panel',
+      contentURL: data.url("service2.html"),
+      contentScriptFile: [
+          data.url("mediatorapi.js"),
+      ],
+      contentScriptWhen: "start",
+      contentScript: contentScript,
+      width: 484, height: 484
+  });
+  return thePanel;
 };
-
 
 exports.test_invoke = function(test) {
   test.waitUntilDone();
@@ -56,26 +62,86 @@ exports.test_invoke = function(test) {
 
     // installing an app makes the dashboard appear, if you don't close it, you get exceptions
     // bug 678238
-    const {dashboard} = require("ui");
-    dashboard._panel.hide();
+    let owa = getOWA();
+    owa._ui._panel.hide();
 
     let services = getOWA()._services;
-    let wm = Cc["@mozilla.org/appshell/window-mediator;1"]
-                .getService(Ci.nsIWindowMediator);
-    let topWindow = wm.getMostRecentWindow("navigator:browser");
-    let medPanel = new TestMediatorPanel(
-      topWindow, getContentWindow(),
-      "test.basic", {hello: "world"}, // serviceName, args
-      function(result) { // success cb
-        test.assertEqual(result.hello, "world");
-        test.done();
-      },
-      function() { // error callback
-        test.fail("error callback invoked");
-      }
-     );
-    services._popups.push(medPanel);
-    medPanel.attachHandlers();
-    medPanel.show();
+    let thePanelRecord = { contentWindow: getContentWindow(),
+                           panel: createTestMediatorPanel(contentScriptSuccess),
+                           methodName: "test.basic",
+                           args: {hello: "world"},
+                           successCB: function(result) {
+                            test.assertEqual(result.hello, "world");
+                            services._popups.pop();
+                            test.done();
+                           },
+                           errorCB: function() {
+                            test.fail("error callback invoked");
+                            services._popups.pop();
+                            test.done();
+                           },
+                           isConfigured: true
+    };
+    services._popups.push(thePanelRecord);
+    services._configureContent(thePanelRecord);
+    services.show(thePanelRecord);
+  });
+};
+
+// A helper for the error tests.
+function testError(test, testType, errchecker) {
+  test.waitUntilDone();
+  installTestApp(test, "apps/basic/basic.webapp", function() {
+    // we don't yet have a "mediator" concept we can use, so we call some
+    // internal methods to set things up bypassing the builtin mediator ui.
+
+    // installing an app makes the dashboard appear, if you don't close it, you get exceptions
+    // bug 678238
+    let owa = getOWA();
+    owa._ui._panel.hide();
+
+    let services = getOWA()._services;
+    let thePanelRecord = { contentWindow: getContentWindow(),
+                           panel: createTestMediatorPanel(contentScriptError),
+                           methodName: "test.basic",
+                           args: {type: testType},
+                           successCB: function(result) {
+                            services._popups.pop();
+                            errchecker(result);
+                           },
+                           errorCB: function(errob) {
+                            test.fail("error callback invoked");
+                            services._popups.pop();
+                            test.done();
+                           },
+                           isConfigured: true
+    };
+    services._popups.push(thePanelRecord);
+    services._configureContent(thePanelRecord);
+    services.show(thePanelRecord);
+  });
+}
+
+exports.test_invoke_error_explicit_ob = function(test) {
+  testError(test, "explicit_errob", function(errob) {
+    test.assertEqual(errob.code, "testable_error");
+    test.assertEqual(errob.message, "a testable error");
+    test.done();
+  });
+};
+
+exports.test_invoke_error_explicit_params = function(test) {
+  testError(test, "explicit_params", function(errob) {
+    test.assertEqual(errob.code, "testable_error");
+    test.assertEqual(errob.message, "a testable error");
+    test.done();
+  });
+};
+
+exports.test_invoke_error_implicit_string_exception = function(test) {
+  testError(test, "implicit_string_exception", function(errob) {
+    test.assertEqual(errob.code, "runtime_error");
+    test.assertEqual(errob.message, "a testable error");
+    test.done();
   });
 };
